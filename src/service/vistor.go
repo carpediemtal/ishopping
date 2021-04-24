ea79f4b50eaa65eb1b757c40332dcf3e58b583d5
ming <674602921@qq.com>
2021-04-24 10:23:59 +0800

    格式

--- src/service/vistor.go
+++ src/service/vistor.go
@@ -6,51 +6,52 @@ import (
 	"ishopping/src/db"
 )
 
-var page_max_limits int = 15 //页面最大展示商品数量
+const PageMaxLimits = 15 //页面最大展示商品数量
 
 type SearchedCommodity struct {
-	Cid          int     `json:"cid" map:"cid"`
-	Name         string  `json:"name" map:"name"`
-	Price        float64 `json:"price" map:"price"`
-	Picture_path string  `json:"picture_path" map:"picture_path"`
+	Cid         int     `json:"cid" map:"cid"`
+	Name        string  `json:"name" map:"name"`
+	Price       float64 `json:"price" map:"price"`
+	PicturePath string  `json:"picture_path" map:"picture_path"`
 }
 
-//通过cid与meta_key查找meta_vul
-func GetCommodityExtraInfoByCid(cid int, meta_key string) (meta_val string, err error) {
-	row := db.DB.QueryRow("select meta_val from commodity_meta where cid = ? and meta_key = ?", cid, meta_key)
-	err = row.Scan(&meta_val)
+// 通过cid与meta_key查找meta_val
+func GetCommodityExtraInfoByCid(cid int, metaKey string) (metaVal string, err error) {
+	row := db.DB.QueryRow("select meta_val from commodity_meta where cid = ? and meta_key = ?", cid, metaKey)
+	err = row.Scan(&metaVal)
 	return
 }
 
 //获取某个分类下对应页数的商品列表
-func GetCommodityListByCategoryAndPage(caid int, page int) (commoditylist []SearchedCommodity, err error) {
+func GetCommodityListByCategoryAndPage(caid int, page int) (commodities []SearchedCommodity, err error) {
 	var rows *sql.Rows
 	if caid == 0 {
-		rows, err = db.DB.Query("select cid,name,price from commodity limit ?, ?", (page-1)*page_max_limits, page_max_limits)
+		rows, err = db.DB.Query("select cid, name, price from commodity limit ?, ?", (page-1)*PageMaxLimits, PageMaxLimits)
 	} else {
-		rows, err = db.DB.Query("select cid,name,price from commodity where caid = ? limit ?, ?", caid, (page-1)*page_max_limits, page_max_limits)
+		rows, err = db.DB.Query("select cid, name, price from commodity where caid = ? limit ?, ?", caid, (page-1)*PageMaxLimits, PageMaxLimits)
 	}
 	if err != nil {
 		return
 	}
+
 	var commodity SearchedCommodity
 	for rows.Next() {
 		err = rows.Scan(&commodity.Cid, &commodity.Name, &commodity.Price)
 		if err != nil {
 			return
 		}
-		commodity.Picture_path, err = GetCommodityExtraInfoByCid(commodity.Cid, "picture_path")
+		commodity.PicturePath, err = GetCommodityExtraInfoByCid(commodity.Cid, "picture_path")
 		if err != nil {
 			if err.Error() == "sql: no rows in result set" {
-				commodity.Picture_path = "defult"
+				commodity.PicturePath = "defult"
 				err = nil
 			} else {
 				return
 			}
 		}
-		commoditylist = append(commoditylist, commodity)
+		commodities = append(commodities, commodity)
 	}
-	if len(commoditylist) == 0 {
+	if len(commodities) == 0 {
 		err = errors.New("no commodity found")
 	}
 	return
